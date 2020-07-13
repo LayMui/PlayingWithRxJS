228e8f9ac33e9941530a2cb75bef9540e7667bb8
Lay Mui <mui_ume@hotmail.com>
2020-07-13 16:17:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+var button = document.querySelector('button');
+button.addEventListener('click', (event) => { 
+  console.log(event);
+});
+
+
+/*
+var observer = {
+  next: function(value) {
+    console.log(value);
+  },
+  error: function(error) {
+    console.log(error);
+  },
+  complete: function() {
+    console.log('Completed');
+  }
+}
+*/
+
+//Rx.Observable.fromEvent(button, 'click').subscribe(observer);
